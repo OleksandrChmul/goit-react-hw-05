27e72b94999d0cc7ad5b43ce725dfdff73ea4b21
Oleksandr Chmul <al.chmul@gmail.com>
2024-03-15 13:26:13 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {}
+import Navigation from './components/Navigation/Navigation';
+import NotFoundPage from './pages/NotFoundPage';
+import Loading from './components/Loading/Loading';
 
-export default App
+const HomePage = lazy(() => import('./pages/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'));
+const MoviesCast = lazy(() => import('./components/MovieCast/MovieCast'));
+const MoviesRewiews = lazy(() =>
+  import('./components/MovieReviews/MovieReviews')
+);
+
+export default function App() {
+  return (
+    <>
+      <Navigation />
+
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MoviesCast />} />
+            <Route path="reviews" element={<MoviesRewiews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
